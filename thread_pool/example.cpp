16a69019b2aa8f7618a6ccb187f9f1066fe78e22
Sascha Wombacher <sascha.wombacher@gmx.de>
2019-02-10 11:59:23 +0100

    add example for threadpool priorities

--- thread_pool/example.cpp
+++ thread_pool/example.cpp
@@ -1,17 +1,67 @@
-#include <iostream>
 #include "threadpool.hpp"
+#include <iostream>
 
-int main(){
-    ThreadPool<> pool;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::future<int> future0 = pool.dispatchWork([]{ std::cout << "hello world" << std::endl; return 0; });
-    std::future<int> future1 = pool.dispatchWork([]{ std::cout << "hello world" << std::endl; return 0; });
-    std::future<int> future2 = pool.dispatchWork([](int val){ std::cout << "hello world " << val << std::endl; return 0; }, 2);
-    pool.dispatchWork([]{ std::cout << "yes" << std::endl; });
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    std::cout << future0.get() << std::endl;
-    std::cout << future1.get() << std::endl;
-    std::cout << future2.get() << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+static void exampleWithoutPriority() {
+    using namespace std::chrono_literals;
+    ThreadPool pool;
+    std::this_thread::sleep_for(1s);
+
+    // example with return statements
+    auto future0 = pool.dispatchWork([] {
+        std::cout << "hello world 0" << std::endl;
+        return 0;
+    });
+    auto future1 = pool.dispatchWork(
+        [](int val) {
+            std::cout << "hello world " << val << std::endl;
+            return val;
+        },
+        1);
+
+    // example without return statement
+    // no future is returned!
+    auto functionNoReturn = [] {
+        std::cout << "hello world no return" << std::endl;
+    };
+    pool.dispatchWork(functionNoReturn);
+    static_assert(
+        std::is_same_v<decltype(pool.dispatchWork(functionNoReturn)), void>,
+        "Non Void return");
+
+    future0.wait();
+    future1.wait();
+    std::cout << "Return values: " << future0.get() << ", " << future1.get()
+              << std::endl;
+}
+
+static void exampleWithPriority() {
+    using namespace std::chrono_literals;
+    ThreadPool pool(1);
+
+    // dispatch some work that take a bit longer,
+    // so we can add low/high priority tasks
+    pool.dispatchWork([] { std::this_thread::sleep_for(100ms); });
+
+    auto future0 = pool.dispatchWork(0, [] {
+        std::cout << "Low Priority job\n" << std::flush;
+        return 0;
+    });
+    auto future1 = pool.dispatchWork(1, [] {
+        std::cout << "High Priority job\n" << std::flush;
+        return 0;
+    });
+
+    // futures return by the threadpool are not blocking on there destructors,
+    // therefore we have to explicitly wait for those
+    future0.wait();
+    future1.wait();
+
+    // note: the threadpools destrutor ignores all currently dispatched tasks
+    // which aren't running, however it will finish the currently running tasks
+}
+
+int main() {
+    exampleWithoutPriority();
+    exampleWithPriority();
     return 0;
 }
